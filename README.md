fff55e0b855637399c9aeacec8532890576afc3f
Martijn van der Kleijn <martijn.niji@gmail.com>
2023-06-05 17:57:51 +0200

    Update licensing information

--- README.md
+++ README.md
@@ -91,4 +91,5 @@ func main() {
 
 # Licensing
 
-Go-simplequeue is licensed under GPLv3. The full details are available from the [LICENSE](/LICENSE) file.
+Go-simplequeue is made available under the [MPL-2.0](https://choosealicense.com/licenses/mpl-2.0/)
+license. The full details are available from the [LICENSE](/LICENSE) file.
--- queues.go
+++ queues.go
@@ -1,3 +1,13 @@
+/*
+	Copyright (C) 2023  Martijn van der Kleijn
+
+	This file is part of the go-simplequeue library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+  	License, v. 2.0. If a copy of the MPL was not distributed with this
+  	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 package simplequeue
 
 import (
